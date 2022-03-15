9da5b6ca3a29f66099d4d9448ec420ebb80c6256
Laurence Qi <laurence.qi@outlook.com>
2022-03-16 07:15:48 +1100

    formatted code

--- grahpql/resolvers/nft.js
+++ grahpql/resolvers/nft.js
@@ -1,88 +1,82 @@
 //RESOLVERS
-//For each query or mutuation there is a resolver, which processes any sort of logic 
+//For each query or mutuation there is a resolver, which processes any sort of logic
 const Nft = require('../../models/Nft');
 const Fight = require('../../models/Fight');
 const checkAuth = require('../../middleware/checkAuth');
 const { AuthenticationError } = require('apollo-server');
 const { getCurrentTournament } = require('./tournament');
-const { UserInputError } = require('apollo-server')
- 
+const { UserInputError } = require('apollo-server');
 
-const organiseFight = async function(nftId){
-    //const  getTourney  = await getCurrentTournament();
-    try {
-      const  fight = await Fight.findOne({ nfts: { $size: 1 } }) || await Fight.findOne({ nfts: { $size: 0 } })
-      //TODO: Instead of throwing new error we need to  trigger a new tournament and change 
-      //current tournament to "ACTIVE"
-      if(!fight) throw new UserInputError('Tournament is full')
-      fight.nfts.push(nftId)
-      fight.save(); 
-    } catch (error) {
-      throw new UserInputError(error)
-    }
-      
-} 
-organiseFight('62305a5fce72d28cb91f5343'); 
+const organiseFight = async function (nftId) {
+  //const  getTourney  = await getCurrentTournament();
+	try {
+		const fight =
+		(await Fight.findOne({ nfts: { $size: 1 } })) ||
+		(await Fight.findOne({ nfts: { $size: 0 } }));
+		//TODO: Instead of throwing new error we need to  trigger a new tournament and change
+		//current tournament to 'ACTIVE'
+		if (!fight) throw new UserInputError('Tournament is full');
+		fight.nfts.push(nftId);
+		fight.save();
+	} catch (error) {
+		throw new UserInputError(error);
+	}
+};
+organiseFight('62305a5fce72d28cb91f5343');
 
-module.exports =  {
+module.exports = {
+	Query: {
+		async getNfts() {
+		try {
+			const result = await Nft.find();
+			return result;
+		} catch (error) {
+			throw new Error(error);
+		}
+		},
 
-    Query: {
-        
-        async getNfts() {
-            try {
-                const result = await Nft.find();
-                return result
-            } catch (error) {
-                throw new Error(error);
-            }
-        },
+		async getNft(_, { nftID }) {
+			try {
+				const result = await Nft.findById(nftID);
+				if (result) {
+				return result;
+				} else {
+				throw new Error('Nft not found');
+				}
+			} catch (error) {
+				throw new Error('Nft not found');
+			}
+		},
+	},
 
-        async getNft(_,{nftID}) {
-            try {
-                const result =  await Nft.findById(nftID);
-                if (result){
-                    return result
-                }else{
-                    throw new Error('Nft not found')
-                }
-                
-            } catch (error) {
-                throw new Error('Nft not found')
-            }
-        },
+	Mutation: {
+		async createNft(_, { createNft }, context) {
+			try {
+				const nft = await Nft({ ...createNft });
+				await nft.save();
 
-    },
+				return nft;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
 
-    Mutation: {
-        async createNft(_, {createNft}, context){
-          try{
-            const nft = await Nft({...createNft})
-            await nft.save();
+		async mintNft(_, { userId }, context) {
+		try {
+				const nft = await Nft.findOne({ user: { $exists: false } });
 
-            return nft;
-          } catch(err){
-            throw new Error(err);
-          }
-        },
-        
-        async mintNft(_, {userId}, context){
-          try{
-            const nft = await Nft.findOne({user: { $exists: false }});
-            if(nft) {
-              nft.user = userId; // this saves a reference to the User with the 'userId'
-              await nft.save();
-              await nft.populate('user'); // adds the user reference obj
-              //organiseFight(nft)
-              return nft;
-            } else {
-              throw new Error('We are out of NFTs');
-            }
-            
-
-          } catch(err){
-            throw new Error(err);
-          }
-        },
-    },
-
-}
+				if (nft) {
+					nft.user = userId; // this saves a reference to the User with the 'userId'
+					await nft.save();
+					await nft.populate('user'); // adds the user reference obj
+					
+					return nft;
+				} else {
+					throw new Error('We are out of NFTs');
+				}
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+	},
+};
