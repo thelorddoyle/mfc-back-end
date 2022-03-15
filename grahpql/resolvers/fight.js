3af47e4a573be830a0c50cbf687191fa93f8581c
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-15 21:49:07 +1100

    Minting now adds nfts to empty fight and check if tournament is full

--- grahpql/resolvers/fight.js
+++ grahpql/resolvers/fight.js
@@ -3,17 +3,7 @@ const Fight = require('../../models/Fight');
 const User = require('../../models/User');
 const checkAuth = require('../../middleware/checkAuth');
 const { AuthenticationError } = require('apollo-server');
-const { getCurrentTournament } = require('./tournament');
 
-
-const organiseFight = async function(){
-    //TODO: Make this work for the championship fight
-    //const  getTourney  = await getCurrentTournament();
-    const  fight = await Fight.findOne({ nfts: { $size: 1 } }) || await Fight.findOne({ nfts: { $size: 0 } })
-    console.log(fight);
-     
-} 
-organiseFight(); 
 module.exports =  {
 
     Query: {
--- grahpql/resolvers/nft.js
+++ grahpql/resolvers/nft.js
@@ -1,11 +1,27 @@
 //RESOLVERS
 //For each query or mutuation there is a resolver, which processes any sort of logic 
 const Nft = require('../../models/Nft');
-const User = require('../../models/User');
+const Fight = require('../../models/Fight');
 const checkAuth = require('../../middleware/checkAuth');
 const { AuthenticationError } = require('apollo-server');
 const { getCurrentTournament } = require('./tournament');
+const { UserInputError } = require('apollo-server')
+ 
 
+const organiseFight = async function(nftId){
+    //const  getTourney  = await getCurrentTournament();
+    try {
+      const  fight = await Fight.findOne({ nfts: { $size: 1 } }) || await Fight.findOne({ nfts: { $size: 0 } })
+      //TODO: Instead of throwing new error we need to create a new tournament
+      if(!fight) throw new UserInputError('Tournament is full')
+      fight.nfts.push(nftId)
+      fight.save(); 
+    } catch (error) {
+      throw new UserInputError(error)
+    }
+      
+} 
+organiseFight('62305a5fce72d28cb91f5343'); 
 
 module.exports =  {
 
@@ -53,10 +69,9 @@ module.exports =  {
             const nft = await Nft.findOne({user: { $exists: false }});
             if(nft) {
               nft.user = userId; // this saves a reference to the User with the 'userId'
-
               await nft.save();
               await nft.populate('user'); // adds the user reference obj
-  
+              //organiseFight(nft)
               return nft;
             } else {
               throw new Error('We are out of NFTs');
