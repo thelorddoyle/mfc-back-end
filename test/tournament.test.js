bb5face0f92ce818538b3c13641e064d238c0ebf
Laurence Qi <laurence.qi@outlook.com>
2022-03-17 18:49:11 +1100

    added chai, mocha and a test

---
+++ test/tournament.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+const url = `http://localhost:4000/`;
+const request = require('supertest')(url);
+
+describe('Tournment', () => {
+    it('should pick a random ')
+});
