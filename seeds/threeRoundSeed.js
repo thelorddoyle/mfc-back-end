4cafd03bffe2b887287abfbb2cbb5a94396c201e
Laurence Qi <laurence.qi@outlook.com>
2022-03-19 14:14:46 +1100

    seeds can change number of round to create in a tournament

--- seeds/threeRoundSeed.js
+++ seeds/threeRoundSeed.js
@@ -5,6 +5,7 @@ const Tournament = require("../models/Tournament");
 const { createTournament } = require("../grahpql/resolvers/tournament");
 const { generateToken } = require("../grahpql/resolvers/user");
 const { mintNft } = require("../grahpql/resolvers/nft");
+const nft = require("../grahpql/resolvers/nft");
 
 const background = ["Blue", "Red", "Cage", "Press Conference", "Gym"];
 const bodyType = ["Ectomorph", "Endomorph", "Mesomorph"];
@@ -27,26 +28,61 @@ const selectRandomElement = function (array) {
     ];
 };
 
+// if you want a variable number of rounds calculate the total number of tournaments. 
+const getTournamentCount = (numRounds) => {
+    // get the sum of 2^0 + 2^1 up till round count
+    let tournamentCount = 0;
+
+    for (let i = 0; i < numRounds; i++){
+        tournamentCount += Math.pow(2, i);
+    }
+
+    return tournamentCount;
+}
+
+const getRoundNum = (tournamentIndex) => {
+    let maxTournamentIndexInRound = 1;
+    
+    for (i = 0; i < 10; i ++){
+        if (tournamentIndex <= maxTournamentIndexInRound){
+            return round = i + 1;
+        } else{
+            maxTournamentIndexInRound += Math.pow(2, i+1);
+        }   
+    }
+}
+
+//TESTS
+// console.log(`for 3 round, count should be: ${getTournamentCount(3)} = 7` );
+// console.log(`for 4 round, count should be: ${getTournamentCount(4)} = 15` );
+// console.log(`for 10 round, count should be: ${getTournamentCount(10)} = 15` );
+
+
+// console.log(`for fight number: '1', round should be: ${getRoundNum(1)} = 1` );
+// console.log(`for fight number: '2', round should be: ${getRoundNum(2)} = 2` );
+// console.log(`for fight number: '3', round should be: ${getRoundNum(3)} = 2` );
+// console.log(`for fight number: '4', round should be: ${getRoundNum(4)} = 3` );
+// console.log(`for fight number: '6', round should be: ${getRoundNum(6)} = 3` );
+// console.log(`for fight number: '7', round should be: ${getRoundNum(7)} = 3` );
+// console.log(`for fight number: '127', round should be: ${getRoundNum(127)} = 7` );
+// console.log(`for fight number: '128', round should be: ${getRoundNum(128)} = 8` );
+
+
 db.once("open", async () => {
     await Nft.deleteMany();
     await Fight.deleteMany();
     await Tournament.deleteMany();
 
-    //TODO: do a login and print the token
-    //TODO: make 4 NFTs 
-    
     //CREATE TOURNAMENTS (THIS TRIGGERS FIGHT CREATION)
-    try {
-        for (let i = 0; i < 7; i++) {
-            let round;
+    try { 
+        const numRounds = 3
+        for (let i = 1; i <= getTournamentCount(numRounds); i++) { // TODO: change the 7 for however many rounds we have. 
+
+            const round = getRoundNum(i)
+            console.log('round found')
             
-            if (i === 0) {
-                round = 1;
-            } else if (i === 1 || i <= 2) {
-                round = 2;
-            } else if (i === 3 || i <= 7) {
-                round = 3;
-            }
+            
+            // for all fights find their respective 
 
             await createTournament({
                 startDate: new Date(),
@@ -97,7 +133,7 @@ db.once("open", async () => {
         })
 
         console.log("Logged in to 'laurence'")
-        console.log('login token:', login)
+        console.log('login token:', login, '(MAY BE A VALID TOKEN)');
     } catch (err){
         console.log(err)
     }
@@ -105,11 +141,10 @@ db.once("open", async () => {
     // MINTING NFTS
     try {
         for(let i = 0; i < 5; i++ ){
-            mintNft("62351e170334c980d7a09953") //TODO: This is not 
+            await mintNft("62351e170334c980d7a09953")
         }
-        // console.log(Nft.findOne())
 
-        // console.log(`Minted ${await Nft.count({user: '62351e170334c980d7a09953'})} Nfts`);
+        console.log(`Minted ${await Nft.count({user: '62351e170334c980d7a09953'})} Nfts that belong to user: 'laurence'`);
 
     } catch (err) {
         throw new Error(err)
