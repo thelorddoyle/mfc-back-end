9e4248f4d5af2f9886cd994a910ddbd3977fd782
Laurence Qi <laurence.qi@outlook.com>
2022-03-25 10:16:33 +1100

    made testing for nft & tournament methods

---
+++ test/nft.test.js
@@ -0,0 +1,57 @@
+const chai = require('chai');
+const assert = require('assert');
+const {isFinalFightFilled, findIfEmptySlotAvailible} = require('../grahpql/resolvers/nft');
+
+describe('check if Final Fight filled returning correct response', () => {
+    it('Should return true', function (){
+        const fights = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {nfts: [0, 0]}]
+        assert.equal(isFinalFightFilled(fights), true )
+    })
+    it('Should return false', function (){
+        const fights = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {nfts: [0]}]
+        assert.equal(isFinalFightFilled(fights), false)
+    })
+});
+
+describe('Checks if findIfEmptySlotAvailible() returns correct function when round = 1', () => {
+    const isFinalFightFilled = findIfEmptySlotAvailible(1);
+    it('should return a function that returns true when slotsOccupied equals 0', function (){
+        assert.equal(isFinalFightFilled(0), true )
+    })
+    it('should return a function that returns true when slotsOccupied equals 1', function (){
+        assert.equal(isFinalFightFilled(1), true )
+    })
+    it('should return a function that returns false when slotsOccupied equals 2', function (){
+        assert.equal(isFinalFightFilled(2), false )
+    })
+
+
+});
+
+describe('Checks if findIfEmptySlotAvailible() returns correct function when round > 1', () => {
+    const isFinalFightFilled = findIfEmptySlotAvailible(2);
+    it('should return true if only 1 slot filled', function (){
+        assert.equal(isFinalFightFilled(1), true )
+    })
+    it('should return false if 0 slots filled', function (){
+        assert.equal(isFinalFightFilled(0), false )
+    })
+    it('should return false if 2 slots filled', function (){
+        assert.equal(isFinalFightFilled(2), false )
+    })
+
+
+});
+
+describe('Finds first empty slot', () => {
+    const isFinalFightFilled = findIfEmptySlotAvailible(2);
+    it('should return true if only 1 slot filled', function (){
+        assert.equal(isFinalFightFilled(1), true )
+    })
+    it('should return false if 0 slots filled', function (){
+        assert.equal(isFinalFightFilled(0), false )
+    })
+    it('should return false if 2 slots filled', function (){
+        assert.equal(isFinalFightFilled(2), false )
+    })
+});
--- test/tournament.test.js
+++ test/tournament.test.js
@@ -1,9 +1,33 @@
 const chai = require('chai');
+const assert = require('assert');
+const {getTier} = require('../grahpql/resolvers/tournament');
+
 
 const expect = chai.expect;
 const url = `http://localhost:4000/`;
 const request = require('supertest')(url);
 
-describe('Tournment', () => {
-    it('should pick a random ')
+describe('Getting correct Tier', () => {
+    it('Should return a value of 1', function (){
+        assert.equal(getTier(15), 1)
+    })
+    it('Should return a value of 2', function (){
+        assert.equal(getTier(16), 2)
+    })
+    it('Should return a value of 2', function (){
+        assert.equal(getTier(23), 2)
+    })
+    it('Should return a value of 3', function (){
+        assert.equal(getTier(24), 3)
+    })
+    it('Should return a value of 4', function (){
+        assert.equal(getTier(28), 4)
+    })
+    it('Should return a value of 5', function (){
+        assert.equal(getTier(30), 5)
+    })
+    it('Should return a value of undefined', function (){
+        assert.equal(getTier(31), undefined)
+    })
 });
+
