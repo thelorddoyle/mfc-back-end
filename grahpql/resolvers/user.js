9f0bf9eefd4ef078a30790e7f8458dedcac66172
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-26 18:41:59 +1100

    adding mutations and types for updatePassword

--- grahpql/resolvers/user.js
+++ grahpql/resolvers/user.js
@@ -254,23 +254,24 @@ module.exports = {
 
         
         async updatePassword(_,{ user }, context){
-                const { id } = checkAuth(context);
-                //Find user
-                const currentUser =  await User.findById(id);
-                //Destructing parameters
-                const { currentPassowrd, password, confirmPassword } = user;
+            const { id } = checkAuth(context);
+            //Find user
+            const currentUser =  await User.findById(id);
+            console.log(currentUser);
+            //Destructing parameters
+            
+            const { currentPassword, password, confirmPassword } = user;
+            //Validate password and current match, plus input fields
+            const {errors, valid } =  await  validateUpdatePassowrd(currentUser.password, currentPassword, password, confirmPassword)
+            if (!valid) throw new UserInputError("Errors", { errors }); 
+            //Hashing new password
+            const newPassword =  await bcrypt.hash(password, 12);
 
-                //Validate password and current match, plus input fields
-                const {errors, valid }=  await  validateUpdatePassowrd(currentUser.password, currentPassowrd, password, confirmPassword)
-                if (!valid) throw new UserInputError("Errors", { errors }); 
-                //Hashing new password
-                const newPassword =  await bcrypt.hash(password, 12);
-
-                //Updating user password
-                currentUser.password = newPassword;
-                await currentUser.save()
-
-                return currentUser;
+            //Updating user password
+            currentUser.password = newPassword;
+            await currentUser.save()
+            return currentUser;
+               
 
         },
 
