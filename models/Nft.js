364ff3109257f3c3819bc3ffd3b6e641f1337e07
Laurence Qi <laurence.qi@outlook.com>
2022-03-28 10:47:49 +1100

    removed ranking reference in Nft

--- models/Nft.js
+++ models/Nft.js
@@ -21,10 +21,6 @@ const nftSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  ranking:{
-    type: Schema.Types.ObjectId,
-    ref: 'Ranking'
-  },
   fights:[{
     type: Schema.Types.ObjectId,
     ref: 'Fight'
